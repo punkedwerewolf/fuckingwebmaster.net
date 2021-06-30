17c0ce8cb54c8c40d8b32fd0759535c71c03c56e
guido <nevermind.the.bollocks28@gmail.com>
2021-06-30 16:16:37 -0700

    d

---
+++ public/audio.js
@@ -0,0 +1,8 @@
+
+window.addEventListener('load', (event) => {
+  let createAudio = `
+<audio id="myAudio" autoplay controls>
+<source src="/tunes/torture.mp3">
+        `;
+    myDiv.innerHTML = createAudio;
+});
